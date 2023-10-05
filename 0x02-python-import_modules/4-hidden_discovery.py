7eccd6195a9c8f4fb8e58bdb511f93bae7049945
oseijoel6111 <oseijoel6111@gmail.com>
2023-10-05 01:35:31 -0700

    Hidden Discovery

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """Print all names defined by hidden_4 module."""
+    import hidden_4
+
+    names = [name for name in dir(hidden_4) if not name.startswith("__")]
+    names.sort()
+
+    for name in names:
+        print(name)
