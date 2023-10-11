0be295d090b7ead2fb0bcdb7d730e442dcc7d725
oseijoel6111 <oseijoel6111@gmail.com>
2023-10-11 13:31:57 -0700

    common elements

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def common_elements(set_1, set_2):
+    # Use the intersection method to find common elements between the two sets
+    common_set = set_1.intersection(set_2)
+    return common_set
