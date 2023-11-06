a4c8d942009c97508a7839947c541efad0b91182
oseijoel6111 <oseijoel6111@gmail.com>
2023-11-06 13:01:53 -0800

    Square 11

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+class Square(BaseGeometry):
+    """
+    A Square class shape, inheirts from BaseGeometry
+    """
+    def __init__(self, size):
+        """"
+        Init function for Square
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def __str__(self):
+        """
+        str funtion to print with/height
+        """
+        return '[Square] ' + str(self.__size) + '/' + str(self.__size)
+
+    def area(self):
+        """
+        A function that calculates the area of the Square
+        """
+        return self.__size ** 2
