7ae9459669c6a1a45dc3e5d62d47dcec4068b49f
oseijoel6111 <oseijoel6111@gmail.com>
2023-11-07 08:28:28 -0800

    Read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Defines a text function."""
+
+def read_file(filename=""):
+    """Print the contents of a UTF8 text file."""
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
---
+++ 0x0B-python-input_output/README.md
@@ -0,0 +1,1 @@
+working with 0x0B-python-input_output
