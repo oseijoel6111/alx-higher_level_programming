236e38132622dcfc35a76b1bde5457ffcfb19927
oseijoel6111 <oseijoel6111@gmail.com>
2023-11-07 11:53:36 -0800

    Student to JSON with filter

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Defines a class Student."""
+
+
+class Student:
+    """Represent a student."""
+
+    def __init__(self, user_first_name, user_last_name, user_age):
+        """Initialize a new Student.
+        """
+        self.first_name = user_first_name
+        self.last_name = user_last_name
+        self.age = user_age
+
+    def to_json(self, attrs=None):
+        """Get a dictionary representation of the Student.
+        """
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
